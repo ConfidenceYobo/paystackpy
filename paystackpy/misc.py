ca4191ffecafb72bb3359724a6bf26f3fce73973
Confi Yobo <confiyobo@gmail.com>
2019-08-30 14:13:44 +0100

    Added balance to misc

--- paystackpy/misc.py
+++ paystackpy/misc.py
@@ -5,3 +5,7 @@ class Misc(APIConfig):
     def allbanks(self, perpage=50, page=1):
         url = self._url("/bank?perPage={}&page={}".format(perpage, page))
         return self._handle_request('GET', url)
+
+    def balance(self):
+        url = self._url("/balance")
+        return self._handle_request('GET', url)
--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ConfiYobo/paystackpy",
     packages=setuptools.find_packages(),
+    download_url='https://github.com/ConfiYobo/paystackpy/archive/0.03.tar.gz',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
